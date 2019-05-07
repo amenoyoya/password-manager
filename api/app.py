e5e51aae698cd9ebd512bf982bb684c4c64271f1
amenoyoya <mtyswe3@gmail.com>
2019-05-07 23:26:04 +0900

    190507

--- api/app.py
+++ api/app.py
@@ -1,17 +1,21 @@
-import flask
+from flask import Flask, request
+from flask_login import login_user, logout_user
 
 # Flaskサーバーアプリケーションの作成
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def index():
-  # JSONデータを返す
-  response = {
-    'success': False,
-    'Content-Type': 'application/json',
-    'data': 'Hello, world'
-  }
-  return flask.jsonify(response)
+@app.route('/login', methods=['POST'])
+def login():
+  try:
+    if request.form['username'] == 'user' and request.form['password'] == 'password':
+      login_user('user')
+  except:
+    response = {
+      'success': False,
+      'Content-Type': 'application/json',
+      'data': 'Hello, world'
+    }
+    return flask.jsonify(response)
 
 if __name__ == '__main__':
   # Flaskサーバー実行
