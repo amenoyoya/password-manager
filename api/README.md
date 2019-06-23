6292fa02f92a2c507e65cd63b3bec8b73d0699ad
amenoyoya <mtyswe3@gmail.com>
2019-06-23 21:25:21 +0900

    Add｜パスワード情報修正・削除API

--- api/README.md
+++ api/README.md
@@ -67,3 +67,63 @@
         ```bash
         $ curl -b cookie http://localhost:4000
         ```
+- **Insert Password Information**
+    - URL: POST http://localhost:4000
+    - Requirement:
+        - Authentication
+    - Request:
+        - category_id: `挿入先のカテゴリーID` (必須)
+        - service: `サービス名` (必須)
+        - username: `ユーザー名`
+        - password: `パスワード`
+        - remarks: `備考`
+    - Response:
+        - 201: `Succeeded to insert data`
+        - 400:
+            - `Require 'category_id' and 'service'` (必須項目が入力されていない)
+            - `Invalid 'category_id'` (ログインユーザー所有のカテゴリーIDではない)
+        - 401: `Unauthenticated` (未認証)
+        - 500: `Failed to insert data`
+    - Usage:
+        ```bash
+        $ curl -b cookie -X POST \
+          -d 'category_id=カテゴリーID' -d 'service=サービス名' \
+          -d 'username=ユーザー名' -d 'password=パスワード' -d 'remarks=備考' http://localhost:4000
+        ```
+- **Update Password Information**
+    - URL: PUT http://localhost:4000/:data_id
+    - Requirement:
+        - Authentication
+    - Request:
+        - service: `サービス名` (必須)
+        - username: `ユーザー名`
+        - password: `パスワード`
+        - remarks: `備考`
+    - Response:
+        - 200: `Succeeded to update data`
+        - 400:
+            - `Require 'service'` (必須項目が入力されていない)
+            - `Nonexistent data` (存在しないデータ)
+            - `Invalid 'data_id'` (ログインユーザー所有のデータIDではない)
+        - 401: `Unauthenticated` (未認証)
+        - 500: `Failed to update data`
+    - Usage:
+        ```bash
+        $ curl -b cookie -X PUT -d 'service=サービス名' \
+          -d 'username=ユーザー名' -d 'password=パスワード' -d 'remarks=備考' http://localhost:4000/データID
+        ```
+- **Delete Password Information**
+    - URL: DELETE http://localhost:4000/:data_id
+    - Requirement:
+        - Authentication
+    - Response:
+        - 200: `Succeeded to delete data`
+        - 400:
+            - `Nonexistent data` (存在しないデータ)
+            - `Invalid 'data_id'` (ログインユーザー所有のデータIDではない)
+        - 401: `Unauthenticated` (未認証)
+        - 500: `Failed to update data`
+    - Usage:
+        ```bash
+        $ curl -b cookie -X DELETE http://localhost:4000/データID
+        ```
--- api/server.py
+++ api/server.py
@@ -38,6 +38,15 @@ class AuthUser:
     def load(session_id):
         return json.loads(session_id)
 
+# encrypt text by using xor
+def encrypt(src_text, key='key'):
+    xor_code = key
+    while len(src_text) > len(xor_code):
+        xor_code += key
+    return ''.join(
+        [chr(ord(data) ^ ord(code)) for (data, code) in zip(src_text, xor_code)]
+    ).encode().hex()
+
 # decrypt text by using xor
 def decrypt(hex_text, key='key'):
     try:
@@ -57,23 +66,23 @@ DATABASE = os.path.join(os.path.dirname(__file__), 'database', 'password.db')
 # Flaskアプリケーション
 app = Frasco(__name__, User=AuthUser)
 
+# Databaseのconnectionを取得
 def get_db():
-    ''' Databaseのconnectionを取得 '''
     if not hasattr(g, 'databse'):
         g.database = SqlDB(DATABASE)
     return g.database
 
+# 認証処理
 @app.auth('/login', '/logout', Response.text('Logged out\n'))
 def login(user):
-    ''' 認証処理 '''
     if user is None:
         return Response.text('Authentication error\n', 400)
     return Response.text('Logged in\n')
 
+# ログインユーザーのパスワード情報を取得
 @app.get('/')
 @app.secret(Response.text('Unauthenticated\n', 401))
 def get_info():
-    ''' ログインユーザーのパスワード情報を取得 '''
     db = get_db()
     res = {}
     categories = db.get_rows('user_categories', {
@@ -91,16 +100,113 @@ def get_info():
             row['username'] = decrypt(row['username'])
     return Response.json(res)
 
+# password と remarks の暗号化解除
 @app.get('/password')
 @app.secret(Response.text('Unauthenticated\n', 401))
 def get_password():
-    ''' password と remarks の暗号化解除 '''
     password = request.form.get('password', '')
     remarks = request.form.get('remarks', '')
     return Response.json({
         'password': decrypt(password),
         'remarks': decrypt(remarks)
     })
+
+# 指定カテゴリーIDがログインユーザーの所有か確認
+def is_correct_category(db, id):
+    categories = db.get_rows('user_categories', {
+        'where': {
+            'and': [
+                {'=': {'id': id}},
+                {'=': {'user_id': app.current_user['id']}}
+            ]
+        }
+    })
+    if not categories or len(categories) == 0:
+        return False
+    return True
+
+# 指定IDのパスワード情報を取得
+def get_data(db, id):
+    data = db.get_rows('category_passwords', {'where': {'=': {'id': id}}})
+    if not data or len(data) == 0:
+        return False
+    return data[0]
+
+# 新規パスワード情報追加
+@app.post('/')
+@app.secret(Response.text('Unauthenticated\n', 401))
+def insert_data():
+    category_id = request.form.get('category_id')
+    service = request.form.get('service')
+    username = request.form.get('username', '')
+    password = request.form.get('password', '')
+    remarks = request.form.get('remarks', '')
+    if category_id is None or service is None:
+        # カテゴリーID, サービス名は必須
+        return Response.text('Require `category_id` and `service`\n', 400)
+    db = get_db()
+    # カテゴリーの所有ユーザーか確認
+    if not is_correct_category(db, category_id):
+        return Response.text('Invalid `category_id`\n', 400)
+    # データ追加
+    count = db.insert_rows('category_passwords', [
+        ['category_id', 'service', 'username', 'password', 'remarks'],
+        [category_id, encrypt(service), encrypt(username), encrypt(password), encrypt(remarks)]
+    ])
+    if count == 0:
+        return Response.text('Failed to insert data\n', 500)
+    return Response.text('Succeeded to insert data\n', 201)
+
+# パスワード情報修正
+@app.put('/<int:data_id>')
+@app.secret(Response.text('Unauthenticated\n', 401))
+def update_data(data_id):
+    service = request.form.get('service')
+    username = request.form.get('username', '')
+    password = request.form.get('password', '')
+    remarks = request.form.get('remarks', '')
+    if service is None:
+        # サービス名は必須
+        return Response.text('Require `service`\n', 400)
+    db = get_db()
+    # 存在しているデータか確認
+    data = get_data(db, data_id)
+    if not data:
+        return Response.text('Nonexistent data\n', 400)
+    # カテゴリーの所有ユーザーか確認
+    if not is_correct_category(db, data['category_id']):
+        return Response.text('Invalid `data_id`\n', 400)
+    # データ更新
+    count = db.update_rows('category_passwords', {
+        'values': {
+            'service': encrypt(service),
+            'username': encrypt(username),
+            'password': encrypt(password),
+            'remarks': encrypt(remarks)
+        },
+        'where': {'=': {'id': data_id}}
+    })
+    if count == 0:
+        return Response.text('Failed to update data\n', 500)
+    return Response.text('Succeeded to update data\n')
+
+# パスワード情報削除
+@app.delete('/<int:data_id>')
+@app.secret(Response.text('Unauthenticated\n', 401))
+def delete_data(data_id):
+    db = get_db()
+    # 存在しているデータか確認
+    data = get_data(db, data_id)
+    if not data:
+        return Response.text('Nonexistent data\n', 400)
+    # カテゴリーの所有ユーザーか確認
+    if not is_correct_category(db, data['category_id']):
+        return Response.text('Invalid `data_id`\n', 400)
+    # データ削除
+    count = db.delete_rows('category_passwords', {'where': {'=': {'id': data_id}}})
+    if count == 0:
+        return Response.text('Failed to delete data\n', 500)
+    return Response.text('Succeeded to delete data\n')
 
 # Flaskサーバー実行
 if __name__ == "__main__":
